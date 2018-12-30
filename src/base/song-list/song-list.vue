2eeb6fd7d21cf8e77f1ae78d750a36941092ef8b
xuxiaoyin <279238850@qq.com>
2018-12-30 23:16:16 +0800

    'songlist'

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li v-for="(song,index) in songs" :key="index" class="item">
+        <h1 class="name">{{song.name}}</h1>
+        <div class="des">{{getDec(song)}}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    songs: {
+      type: Array,
+      default: []
+    }
+  },
+  methods: {
+    getDec(song){
+      return `${song.singer} . ${song.album}`
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/variable'
+
+.song-list
+  padding: 28px 30px
+  .item
+    margin-bottom: 15px
+    .name
+      line-height: 25px
+      font-size: $font-size-medium-x
+      color: $color-text
+    .des
+      line-height: 25px
+      font-size: $font-size-medium
+      color: $color-text-l
+</style>
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-1"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgstyle">
+    <div class="bg-image" :style="bgstyle" ref="bgImage">
       <div class="filter"></div>
     </div>
+    <div class="top-layre" ref="toplayer"></div>
+    <scroll 
+      :data="songs" 
+      class="list" 
+      ref="list" 
+      :probe-type="probeType" 
+      :listen-scroll="listenScroll"
+      @scroll="scroll">
+      <div class="song-list-wrap">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+import SongList from 'base/song-list/song-list'
+import Scroll from 'base/scroll/scroll'
+const TOPHEIGTH=40
 export default {
   props: {
     bgImage: {
@@ -26,9 +41,61 @@ export default {
       default: ''
     }
   },
+  mounted() {
+    this.bgImageHeight=this.$refs.bgImage.clientHeight
+    this.minTranslateY=-this.bgImageHeight+TOPHEIGTH
+    this.$refs.list.$el.style.top=`${this.bgImageHeight}px`
+  },
+  created() {
+    this.probeType=3
+    this.listenScroll=true
+  },
+  data() {
+    return {
+      scrollY: 0
+    }
+  },
+  methods: {
+    scroll(pos) {
+      this.scrollY=pos.y
+    },
+    back() {
+      this.$router.back()
+    }
+  },
   computed: {
     bgstyle() {
       return `background-image:url(${this.bgImage})`
+    }
+  },
+  components: {
+    SongList,
+    Scroll
+  },
+  watch: {
+    scrollY(newy) {
+      let translateY=Math.max(this.minTranslateY,newy)
+      let zIndex=0
+      let scale=1
+      this.$refs.toplayer.style['transform']=`translate3d(0,${translateY}px,0)`
+      this.$refs.toplayer.style['webkitTransform']=`translate3d(0,${translateY}px,0)`
+      const percent=Math.abs(newy/this.bgImageHeight)
+      if(newy>0){
+        scale=1+percent
+        zIndex=10
+      }
+      if(newy<this.minTranslateY){
+        zIndex=10
+        this.$refs.bgImage.style.height=`${TOPHEIGTH}px`
+        this.$refs.bgImage.style.paddingTop=0
+      }else{
+        zIndex=0
+        this.$refs.bgImage.style.height=0
+        this.$refs.bgImage.style.paddingTop='70%'
+      }
+      this.$refs.bgImage.style.zIndex=zIndex
+      this.$refs.bgImage.style['transform']=`scale(${scale})`
+      this.$refs.bgImage.style['webkitTransform']=`scale(${scale})`
     }
   }
 }
@@ -46,7 +113,7 @@ export default {
   background: $color-background 
   .back 
     position: absolute
-    z-index: 2 
+    z-index: 30 
     top: 0
     left: 0
     padding: 10px 20px
@@ -55,7 +122,7 @@ export default {
       color: $color-theme 
   .title 
     position: absolute
-    z-index: 2
+    z-index: 20
     width: 100%
     line-height: 40px
     text-align: center
@@ -65,7 +132,9 @@ export default {
     position: relative
     width: 100%
     height: 0
-    padding-top: 60%
+    padding-top: 70%
+    transform-origin: top
+    background-size: cover
     .filter
       position: absolute 
       top: 0
@@ -73,4 +142,15 @@ export default {
       width: 100%
       height: 100%
       background: $color-background-d
+  .list
+    position: absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+  .top-layre
+    position: relative
+    width: 100%
+    height: 100%
+    background: $color-background
 </style>
