298ec84b75fbe69b81889be07a1838825652730b
xuxiaoyin <279238850@qq.com>
2019-01-08 09:44:31 +0800

    'play-mode'

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,17 +2,31 @@ import * as types from './mutation-types'
 import {getNutil} from 'common/js/nutil'
 import {palyMode} from 'common/js/config'
 
+function findIndex(list,song) {
+  return list.findIndex((item) => {
+    return item.id===song.id
+  })
+}
+
 export const selectPlay=function({commit,state},{list,index}){
   commit(types.SET_SEQUENCELIST,list)
-  commit(types.SET_SONGLIST,list)
+  if(state.mode===palyMode.random) {
+    let radomList=getNutil(list)
+    commit(types.SET_SONGLIST,radomList)
+    index=findIndex(radomList,list[index])
+  }else{
+    commit(types.SET_SONGLIST,list)
+  }
   commit(types.SET_CURRENTINDEX,index)
   commit(types.SET_FULLSCREEN,true)
   commit(types.SET_PLAYING,true)
 }
 
 export const redomPlay=function({commit},{list}){
+  commit(types.SET_SEQUENCELIST,list)
   let radomList=getNutil(list)
   commit(types.SET_SONGLIST,radomList)
+  commit(types.SET_MODE,palyMode.random)
   commit(types.SET_CURRENTINDEX,0)
   commit(types.SET_FULLSCREEN,true)
   commit(types.SET_PLAYING,true)
